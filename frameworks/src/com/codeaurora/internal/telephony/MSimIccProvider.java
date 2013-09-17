ecdadf7180fcd19701cded09533bddf2b420a6fe
Maanasa Gopal <magopa@codeaurora.org>
2013-09-17 16:41:26 +0530

    SIM Phone Book : Modify update operation
    Add empty number check during update operation to comply
    with the 3GPP 51.101 sections
    10.5.1 EFADN (Abbreviated dialling numbers) and
    10.5.2 EFFDN (Fixed dialling numbers)
    
    Change-Id: I094a545c3400df8aedd3b16dce5f002e80ebb1e2
    CRs-Fixed: 539346

--- frameworks/src/com/codeaurora/internal/telephony/MSimIccProvider.java
+++ frameworks/src/com/codeaurora/internal/telephony/MSimIccProvider.java
@@ -364,6 +364,11 @@ public class MSimIccProvider extends IccProvider {
         String[] emails = null;
         String newTag = values.getAsString("newTag");
         String newNumber = values.getAsString("newNumber");
+        /*As part of 3GPP 51.011, number field is mandatory while storing in the
+          SIM for both ADN and FDN */
+        if (TextUtils.isEmpty(newNumber)) {
+            return 0;
+        }
         String[] newEmails = null;
         // TODO(): Update for email.
         boolean success = updateIccRecordInEf(efType, tag, number,
