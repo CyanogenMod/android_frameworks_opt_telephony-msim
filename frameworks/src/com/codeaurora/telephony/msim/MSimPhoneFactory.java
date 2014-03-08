e6712a5d6aec0f14523730594251a657d2debc9e
Pawit Pornkitprasan <p.pawit@gmail.com>
2014-03-08 19:39:18 +0700

    MSimPhoneFactory: use custom RIL class
    Change-Id: Idf44a733cbc70e1f64fa0c4932f7a069bd6f449e

--- frameworks/src/com/codeaurora/telephony/msim/MSimPhoneFactory.java
+++ frameworks/src/com/codeaurora/telephony/msim/MSimPhoneFactory.java
@@ -53,6 +53,8 @@ import com.android.internal.telephony.uicc.UiccController;
 import com.android.internal.telephony.PhoneConstants;
 import com.codeaurora.telephony.msim.MSimTelephonyIntents;
 
+import java.lang.reflect.Constructor;
+
 import static com.android.internal.telephony.TelephonyProperties.PROPERTY_DEFAULT_SUBSCRIPTION;
 
 /**
@@ -134,6 +136,10 @@ public class MSimPhoneFactory extends PhoneFactory {
                 sProxyPhones = new MSimPhoneProxy[numPhones];
                 sCommandsInterfaces = new RIL[numPhones];
 
+                // Get RIL class name
+                String sRILClassname = SystemProperties.get("ro.telephony.ril_class", "RIL").trim();
+                Rlog.i(LOG_TAG, "RILClassname is " + sRILClassname);
+
                 for (int i = 0; i < numPhones; i++) {
                     //reads the system properties and makes commandsinterface
                     try {
@@ -154,8 +160,20 @@ public class MSimPhoneFactory extends PhoneFactory {
                     }
 
                     Rlog.i(LOG_TAG, "Network Mode set to " + Integer.toString(networkModes[i]));
-                    sCommandsInterfaces[i] = new RIL(context, networkModes[i],
-                            cdmaSubscription, i);
+                    // Use reflection to construct the RIL class (defaults to RIL)
+                    try {
+                        sCommandsInterfaces[i] = instantiateCustomRIL(
+                                sRILClassname, context, networkModes[i], cdmaSubscription, i);
+                    } catch (Exception e) {
+                        // 6 different types of exceptions are thrown here that it's
+                        // easier to just catch Exception as our "error handling" is the same.
+                        // Yes, we're blocking the whole thing and making the radio unusable. That's by design.
+                        // The log message should make it clear why the radio is broken
+                        while (true) {
+                            Rlog.e(LOG_TAG, "Unable to construct custom RIL class", e);
+                            try {Thread.sleep(10000);} catch (InterruptedException ie) {}
+                        }
+                    }
                 }
 
                 // Instantiate MSimUiccController so that all other classes can just
@@ -215,6 +233,15 @@ public class MSimPhoneFactory extends PhoneFactory {
 
             }
         }
+    }
+
+    private static <T> T instantiateCustomRIL(String sRILClassname, Context context,
+            int networkMode, int cdmaSubscription, Integer instanceId) throws Exception {
+        Class<?> clazz = Class.forName("com.android.internal.telephony." + sRILClassname);
+        Constructor<?> constructor = clazz.getConstructor(
+                Context.class, int.class, int.class,Integer.class);
+        return (T) clazz.cast(constructor.newInstance(
+                context, networkMode, cdmaSubscription, instanceId));
     }
 
     public static Phone getMSimCdmaPhone(int subscription) {
